3cd1598dc07a12bdbb168b1c1d134b261a95b709
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-12-28 13:16:02 -0600

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StreamChat} from 'stream-chat-react'
+import {Chat} from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const App = () => {
+    return (
+        <div>
+            <h1>chat app.</h1>
+        </div>
+    )
+}
+
+export default App
