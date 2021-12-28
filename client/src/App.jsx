3c10ef8cdd4713d17a63b9d8c26650d7f6e9c6ea
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-12-28 16:58:50 -0600

    hot fix .01

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import {StreamChat} from 'stream-chat-react'
-import {Chat} from 'stream-chat-react'
-import Cookies from 'universal-cookie'
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+
+import { ChannelListContainer, ChannelContainer } from './components';
+
+const apiKey = 'e7bem6469h7v';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
     return (
-        <div>
-            <h1>chat app.</h1>
+        <div className='app__wrapper'>
+
+           
+
+            <Chat client={client} theme="team light">
+                <ChannelListContainer
+                
+                />
+                <ChannelContainer
+                
+                />
+            </Chat>
+            
         </div>
     )
 }
