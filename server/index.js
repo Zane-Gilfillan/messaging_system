e4e872c27f103a106d8bae9fecd230f8578a6502
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-01-04 14:11:52 -0600

    server build & route setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const authRoutes = require("./routes/auth.js")
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -10,8 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded);
 
-app.get('/', () => {
-    resizeBy.send('hello everyone')
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
 })
 
 app.listen(PORT, () => console.log(`server is running on ${PORT}`))
