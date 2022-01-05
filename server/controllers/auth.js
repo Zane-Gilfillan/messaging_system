1478bbd7e49fb11c068cbdf8dade8c0574d7ee33
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-01-04 19:36:16 -0600

    added user successful login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -39,6 +39,21 @@ const login = async (req, res) => {
         const ServerClient = connect(api_key, api_secret, app_id);
 
         const client = StreamChat.getInstance(api_key, api_secret);
+
+        const { users } = await client.queryUsers({ name: username});
+
+        if(!users.length) return res.status(400).json({ message: 'User not fountd'});
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+        const token = serverClient.createUserToken(users[0].id);
+
+        if(success) {
+            res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id});
+
+        } else {
+            res.status(500).json({ message: 'incorrect password '});
+        }
     } catch (error) {
         res.status(500).json({ message: error });
     }
