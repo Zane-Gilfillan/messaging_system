8540e19c15289fe2182ab85d4a9c637713f3835a
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-01-04 20:35:05 -0600

    searching for auth bug

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,15 +5,13 @@ const crypto = require('crypto');
 
 require('dotenv').config();
 
-const api_key = process.env.STREAM_API_KEY
-const api_secret = process.env.STREAM_API_SECRET
-const api_id = process.env.STREAM_APP_ID
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_APP_ID;
 
-const signup = async (req, res) => {
-
+const signup = async (req, res) => { 
     try {
-
-        const { fullName, username, password, phoneNumber} = req.body;
+        const { fullName, username, password, phoneNumber } = req.body;
 
         const userId = crypto.randomBytes(16).toString('hex');
 
@@ -23,28 +21,24 @@ const signup = async (req, res) => {
 
         const token = serverClient.createUserToken(userId);
 
-        res.status(200).json({ token, fullName, userId, hashedPassword, phoneNumber })
-        
+        res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber });
     } catch (error) {
-        console.log(error);
+        console.log('the issue is here',error);
 
-        res.status(500).json({ message: error});
+        res.status(500).json({ message: error });
     }
-
-}
-
+};
 
 const login = async (req, res) => {
     try {
-        const { username, password} = req.body;
-
-        const ServerClient = connect(api_key, api_secret, app_id);
-
+        const { username, password } = req.body;
+        
+        const serverClient = connect(api_key, api_secret, app_id);
         const client = StreamChat.getInstance(api_key, api_secret);
 
-        const { users } = await client.queryUsers({ name: username});
+        const { users } = await client.queryUsers({ name: username });
 
-        if(!users.length) return res.status(400).json({ message: 'User not fountd'});
+        if(!users.length) return res.status(400).json({ message: 'User not found' });
 
         const success = await bcrypt.compare(password, users[0].hashedPassword);
 
@@ -52,16 +46,14 @@ const login = async (req, res) => {
 
         if(success) {
             res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id});
-
         } else {
-            res.status(500).json({ message: 'incorrect password '});
+            res.status(500).json({ message: 'Incorrect password' });
         }
-    } catch (error) {
+    } catch (error) {ads
+        console.log(error);
+
         res.status(500).json({ message: error });
     }
-
-}
-
-
+};
 
 module.exports = { signup, login }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+
+   
 const express = require('express');
 const cors = require('cors');
 
-const authRoutes = require("./routes/auth.js")
+const authRoutes = require("./routes/auth.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,12 +12,18 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded);
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+    res.send('Hello, World!');
+});
 
-app.use('/auth', authRoutes)
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body;
 
-app.listen(PORT, () => console.log(`server is running on ${PORT}`))
+    return res.status(200).send('Not a new message request');
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
