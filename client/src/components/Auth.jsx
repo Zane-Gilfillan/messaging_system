cb60e4dd0c18fd190e401c2468ec207a74f1695b
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-01-04 19:43:06 -0600

    added cookie functionality

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg'
 
+const cookies = new Cookies();
+
 const initialState = {
     fullName: '',
     userName: '',
@@ -22,10 +24,28 @@ const Auth = () => {
 
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit =  async (e) => {
         e.preventDefault();
 
-        console.log(form)
+        const { fullName, username, password, phoneNumber, avatarURL } = form;
+
+        const URL = 'http;//localhost:5000/auth';
+
+        const {data: token, userId, hashedPassword} = await axios.post(`${URL}/${isSignup ? 'sign up' : 'login'}`, {
+            username, password, fullName, phoneNumber, avatarURL,
+        })
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('fullName', fullName);
+            cookies.set('hashedPassword', hashedPassword);
+        }
     }
 
     const switchMode =() => {
