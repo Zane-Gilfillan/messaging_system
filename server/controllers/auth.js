d6acf229bef45ee8ffa16e8cb8aad391d0a544d0
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-01-04 19:20:42 -0600

    login in and signup server build

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,50 @@
-const login = () => {
+const { connect } = require('getstream');
+const bcrypt = require('bcrypt');
+const StreamChat = require('stream-chat');
+const crypto = require('crypto');
+
+const api_key = process.env.STREAM_API_KEY
+const api_secret = process.env.STREAM_API_SECRET
+const api_id = process.env.STREAM_APP_ID
+
+const signup = async (req, res) => {
+
+    try {
+
+        const { fullName, username, password, phoneNumber} = req.body;
+
+        const userId = crypto.randomBytes(16).toString('hex');
+
+        const serverClient = connect(api_key, api_secret, app_id);
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const token = serverClient.createUserToken(userId);
+
+        res.status(200).json({ token, fullName, userId, hashedPassword, phoneNumber })
+        
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({ message: error});
+    }
 
 }
 
-const signup = () => {
+
+const login = async (req, res) => {
+    try {
+        const { username, password} = req.body;
+
+        const ServerClient = connect(api_key, api_secret, app_id);
+
+        const client = StreamChat.getInstance(api_key, api_secret);
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
 
 }
+
+
 
 module.exports = { signup, login }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const { signup, login } = require('../controllers/auth.js')
+
 
 
 router.post('/signup');
 router.post('/login')
+
+module.exports = router;
